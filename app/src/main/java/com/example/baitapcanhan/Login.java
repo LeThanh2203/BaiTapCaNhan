31755c741b5205826ec1263fe627224f02948448
lethanhy <lethanhy220304@gmail.com>
2024-11-29 08:10:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baitapcanhan/Login.java
@@ -0,0 +1,33 @@
+package com.example.baitapcanhan;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+Button signInButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+     signInButton = findViewById(R.id.signInButton);
+     signInButton.setOnClickListener(v ->{
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
